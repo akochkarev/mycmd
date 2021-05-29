ed2200a43cb5798a88f7009e733a61112367bdce
alex-k <alex@WINDOWS-9SC4T5C>
2021-05-29 19:17:15 +0700

    case lowering

--- win32-i386-win32-build.cmd
+++ win32-i386-win32-build.cmd
@@ -1,4 +1,4 @@
-fpc -va  OPT="-gl -Xs -WC" -Twin32 -Pi386 -FE.\bin\ .\src\MyCmd.pas >i386-win32.log
+fpc -va  OPT="-gl -Xs -WC" -Twin32 -Pi386 -FE.\bin\ .\src\mycmd.pas >i386-win32.log
 
 xcopy /Y .\src\mycmd.ini .\bin >>i386-win32.log
 xcopy /Y .\src\start.cmd .\bin >>i386-win32.log
